ff6c694a4867e0e775fbc2fae9e066cf900e99fc
raghulshivaa <ragulovely2710@gmail.com>
2022-08-25 12:25:49 +0530

    first push

---
+++ src/test/java/org/project/ProjectClass.java
@@ -0,0 +1,15 @@
+package org.project;
+
+public class ProjectClass {
+	
+	private void method1() {
+		System.out.println(1);
+	}
+private void method2() {
+	System.out.println(2);
+}
+private void method3() {
+	System.out.println(3);
+
+}
+}
